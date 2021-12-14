ec1796f8f504f5a4ff3e6f339a0e751e8e05da76
Marcos Vinícius <marcosvinicius.analucia@gmail.com>
2021-12-14 02:55:56 -0300

    clean repo

--- ChessAI/Assets/Scripts/Abstractions/IObjectTweetner.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Assets.Scripts.Abstractions
-{
-    public interface IObjectTweetner
-    {
-        void MoveTo(Transform transform, Vector3 targetPosition);
-    }
-}
